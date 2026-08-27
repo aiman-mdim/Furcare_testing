d904b40cd768b2968ba0647fc594135111cf171f
Aiman <aimanmohammad594@gmail.com>
2026-08-28 04:17:08 +0600

    Fix deployment

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,4 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  window.location.origin;
+  import.meta.env.VITE_API_URL || window.location.origin;
 
-export default API_BASE_URL;
+export default API_BASE_URL; 
